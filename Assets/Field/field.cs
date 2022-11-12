842238d8f1cb2eab7b68254b140cc9f71f843135
Валерий <49197452+XpomIX@users.noreply.github.com>
2022-11-12 16:48:36 +0500

    NATQ-7 Refactor field code to better workflow (#13)
    * NATQ-7 Refactor field code to better workflow
    
    * NATQ-7 save scene

--- Assets/Field/field.cs
+++ Assets/Field/field.cs
@@ -1,64 +1,55 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-public class field : MonoBehaviour
+public class Field : MonoBehaviour
 {
-    [SerializeField] private int _fieldSize;
-    [SerializeField] private GameObject _fieldCellContainer;
+    [SerializeField] private int _size;
+    [SerializeField] private GameObject _fieldCell;
     [SerializeField] private Color _primaryColor;
     [SerializeField] private Color _secondaryColor;
-    
-    public float GetCellSize()
+
+    private float GetCellSize()
     {
-        return transform.localScale.x / _fieldSize;
+        return 1f / _size;
     }
-    public List<Vector2> GetCellsPositions()
+    private Vector2 NormalizeCellPosition(float x, float y)
     {
         float cellSize = GetCellSize();
-        Vector2 parentScale = transform.parent.transform.localScale;
-        Vector2 leftTopCornerPosition = new Vector2(
-            transform.position.x - parentScale.x / 2,
-            transform.position.y - parentScale.y / 2
-        );
-        List<Vector2> cellsPositions = new List<Vector2>();
-        for (int i = 0; i < _fieldSize; i++)
+        return new Vector2((x + cellSize / 2) * transform.parent.localScale.x, (y - cellSize / 2) * transform.parent.localScale.y);
+    }
+    public List<Vector2> GetCellPositions()
+    {
+        List<Vector2> cellPositions = new List<Vector2>();
+
+        for (int i = 0; i < _size; i++)
         {
-            for (int j = 0; j < _fieldSize; j++)
+            for (int j = 0; j < _size; j++)
             {
-                cellsPositions.Add(new Vector2(leftTopCornerPosition.x + cellSize * j * parentScale.x, leftTopCornerPosition.y + cellSize * (i + 1) * parentScale.y));
+                cellPositions.Add(NormalizeCellPosition(-0.5f + (float)j / _size, 0.5f - (float)i / _size));
             }
         }
-        Debug.Log(cellsPositions.Count);
-        return cellsPositions;
+        return cellPositions;
     }
 
-    private void Start()
+    void Start()
     {
         gameObject.GetComponent<SpriteRenderer>().color = _primaryColor;
         float cellSize = GetCellSize();
-
-        bool isEvenCell = true;
-        foreach(Vector2 cellPosition in GetCellsPositions())
+        List<Vector2> cellPositions = GetCellPositions();
+        int cellsCount = cellPositions.Count;
+        for (int i = 0; i < cellsCount; i++)
         {
-            if(isEvenCell)
-            {
-                GameObject createdCellContainer = Instantiate(
-                    _fieldCellContainer,
-                    new Vector3(cellPosition.x, cellPosition.y, 0.5f),
-                    Quaternion.identity,
-                    gameObject.transform
-                );
-                createdCellContainer.transform.localScale = new Vector2(cellSize, cellSize);
-                GameObject cell = createdCellContainer.transform.GetChild(0).gameObject;
-                cell.GetComponent<SpriteRenderer>().color = _secondaryColor;
-                isEvenCell = false;
-            } else
-            {
-                isEvenCell = true;
-            }
+            int row = i / _size + 1;
+            int col = i % _size;
+
+            if ((row % 2 == 0) && (col % 2 == 0)) continue;
+            if ((row % 2 == 1) && (col % 2 == 1)) continue;
+
+            Vector2 cellPosition = cellPositions[i];
+            GameObject createdCell = Instantiate(_fieldCell, cellPosition, Quaternion.identity, transform);
+            createdCell.GetComponent<SpriteRenderer>().color = _secondaryColor;
+            createdCell.transform.localScale = new Vector3(cellSize, cellSize, 1);
         }
-        
     }
 }
